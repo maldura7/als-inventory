4c926af5de03fd2dc29e09cea514e0064ac90c85
Ahmed Aldura <alduramohammed47@gmail.com>
2025-11-03 12:11:17 -0700

    Add axios dependency and fix duplicate path declaration

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Serve static files from frontend build
-const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve React app for all non-API routes (SPA routing)
